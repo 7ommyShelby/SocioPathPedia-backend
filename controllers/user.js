0f47a0f9c6b6da627c461bef7144e678d724e9f0
7ommyShelby <aryaalsaher@gmail.com>
2024-07-16 23:55:19 +0530

    allusers

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const usermodel = require("../model/User");
 
-
 const getuser = async (req, res) => {
+
     try {
         const { id } = req.params;
         const user = await usermodel.findById(id);
@@ -9,7 +9,7 @@ const getuser = async (req, res) => {
         res.json(user)
 
     } catch (error) {
-        console.log("something went wrong at user", error.message);
+        console.log("something went wrong at user", error);
         res.status(404).json({
             message: "something went wrong at user" + error.message
         })
@@ -32,9 +32,9 @@ const getfriends = async (req, res) => {
                 _id: e._id,
                 firstName: e.firstName,
                 lastName: e.lastName,
-                occupation : e.occupation,
-                location : e.location,
-                picturePath : e.picturePath
+                occupation: e.occupation,
+                location: e.location,
+                picturePath: e.picturePath
             }
         })
 
@@ -84,7 +84,7 @@ const addremovefriends = async (req, res) => {
 
         res.json(friendsdata)
         // res.json(friends)
-        
+
     } catch (error) {
         console.log("something went wrong at updating friendslist", error.message);
         res.status(404).json({
@@ -93,10 +93,29 @@ const addremovefriends = async (req, res) => {
     }
 }
 
+const getall = async (req, res) => {
+
+    try {
+
+        const allusers = await usermodel.find()
+        console.log(allusers);
+        res.json({
+            allusers: allusers
+        })
+
+    } catch (error) {
+        console.log("something went wrong at getting all users", error);
+        res.status(404).json({
+            message: "something went wrong at getting all users" + error.message
+        })
+    }
+}
+
 const usercontroller = {
     getuser,
     getfriends,
     addremovefriends,
+    getall
 }
 
 module.exports = usercontroller
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const validator = require('../middleware/auth')
-const { getuser, getfriends, addremovefriends } = require('../controllers/user')
+const { getuser, getfriends, addremovefriends, getall } = require('../controllers/user')
 
 
-router.get('/:id', getuser)
+router.get('/get/:id', getuser)
 router.get('/:id/friends', getfriends)
+router.get('/getall', getall)
 router.patch('/:id/:friendId/updatefriends', addremovefriends)
 
 module.exports = router
