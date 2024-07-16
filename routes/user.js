cdfec71480aef917f7851a2d55f1b134d0d9608b
7ommyShelby <aryaalsaher@gmail.com>
2024-07-17 01:52:03 +0530

    persnol

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,9 @@ const validator = require('../middleware/auth')
 const { getuser, getfriends, addremovefriends, getall } = require('../controllers/user')
 
 
-router.get('/get/:id', getuser)
+router.get('/details/:id', getuser)
 router.get('/:id/friends', getfriends)
-router.get('/getall', getall)
+router.get('/users', getall)
 router.patch('/:id/:friendId/updatefriends', addremovefriends)
 
 module.exports = router
